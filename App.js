57a0c3009b9de35b8bb3800ca702fd76ca13e3ab
Adrian-Bulambao <adrianjustinbulambao05@gmail.com>
2026-03-19 19:16:29 +0800

    Changed for the hoa

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>"Spyware has been deployed"</Text>
+      <Text>- Adrian Justin Bulambao</Text>
       <StatusBar style="auto" />
     </View>
   );
